387c66a2619d3d39b8d1a570097d0201d15906a1
Mel Krusniak <mel.krusniak@gmail.com>
2026-02-10 14:26:37 -0700

    Re-add DecisionProblems; fix docs

--- DecisionProblems.jl/src/DecisionProblems.jl
+++ DecisionProblems.jl/src/DecisionProblems.jl
@@ -1,35 +1,35 @@
 module DecisionProblems
 
-# using DecisionNetworks
-# using StaticArrays
+using DecisionNetworks
+using StaticArrays
 
-# include("metrics.jl")
-# export DecisionMetric
+include("metrics.jl")
+export DecisionMetric
 
-# export aggregate!
-# export output
-# export reset!
+export aggregate!
+export output
+export reset!
 
-# export Discounted
-# export DiscountedReward
-# export Trace
-# export NIters
+export Discounted
+export DiscountedReward
+export Trace
+export NIters
 
-# include("problems.jl")
-# export DecisionProblem
-# export model
-# export objective
-# export graph
-# export initial
+include("problems.jl")
+export DecisionProblem
+export model
+export objective
+export graph
+export initial
 
-# include("algorithms.jl")
-# export DecisionAlgorithm
-# export solve
-# export simulate!
+include("algorithms.jl")
+export DecisionAlgorithm
+export solve
+export simulate!
 
-# include("named_problems.jl")
-# export MDP
-# export POMDP
-# export MG
+include("named_problems.jl")
+export MDP
+export POMDP
+export MG
 
 end
